87720d8f897f71e273d009a6f6044f33a1b21b45
Caner Budak <62038536+CanerBudak@users.noreply.github.com>
2021-05-18 00:05:55 +0300

    Update Owner.js

--- src/components/participants/Owner.js
+++ src/components/participants/Owner.js
@@ -6,63 +6,72 @@ import OwnerViews from "./OwnerViews";
 
 export class Owner extends Component {
     static contextType = Context;
-    
+
     constructor(props) {
         super(props);
         this.state = {
-            appState:"",
-            args:[],
+            appState: "",
+            args: [],
             resPurchase: null
-            
+
         }
         //bind functions 
-        this.PurchaseExt= this.PurchaseExt.bind(this);
-}
-    componentDidMount(){
-        const[, , , , , ctc ,]= this.context;
-        this.interval = setInterval(async () => await this.UpdateBalance(), 50000)
+        this.purchaseExt = this.purchaseExt.bind(this);
+    }
+    componentDidMount() {
+        console.log(this.context);
+        const [, , , , , , ctc, ,] = this.context;
+        this.interval = setInterval(async () => await this.updateBalance(), 50000)
 
         //Reache bağlan
         Backend.Owner(ctc[0], this);
-}
+    }
 
-    async UpdateBalance(){
+    async updateBalance() {
         const [account, , , setBalance] = this.context;
 
         const balance = Reach.formatCurrency(await Reach.balanceOf(account), 4);
-        setBalance(balance); 
-
-
-}
-    async  purchase(price){
-        const response = await new Promise( res=> {
+        setBalance(balance);
+    }
+    async purchase(price) {
+        const priceFmt = Reach.formatCurrency(price, 4);
+        const response = await new Promise(res => {
             this.setState({
-                appState: "Purchase",
-                args: [res],
+                appState: "purchase",
+                args: [priceFmt],
                 resPurchase: res
             });
         });
         return response;
-}
-    PurchaseExt(response){
+    }
+    purchaseExt(response) {
         this.state.resPurchase(response);
-}
+    }
 
-    async revealId(id){
+    async seeOwner(addr) {
+        const addrFmt = Reach.formatAddress(addr);
         this.setState({
-            appState:"revealId",
-            args: [id],
-        })
-} 
+            appState: "seeOwner",
+            args: [addrFmt]
+        });
+    }
 
- 
+    async revealId(id) {
+        const idFmt = Reach.bigNumberToNumber(id);
+        this.setState({
+            appState: "revealId",
+            args: [idFmt],
+        })
+    }
+
+
     render() {
         return <OwnerViews
             appState={this.state.appState}
-            args = {this.state.args}
-            revealPurchase={this.state.resPurchaseId !== null}
-            revealPurchaseExt={this.revealPurchaseExt} />
-        }
+            args={this.state.args}
+            purchaseReady={this.state.resPurchase !== null}
+            purchase={this.purchaseExt} />
+    }
 }
 
 
