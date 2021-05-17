4b9648debd10fb6ba0676b60f67f6ec5b9e60754
Caner Budak <62038536+CanerBudak@users.noreply.github.com>
2021-05-18 00:05:23 +0300

    Update CreatorViews.js

--- src/components/participants/CreatorViews.js
+++ src/components/participants/CreatorViews.js
@@ -1,56 +1,58 @@
 import React from "react";
-import {parseCurrency} from "@reach-sh/stdlib/ALGO";
+import { parseCurrency } from "@reach-sh/stdlib/ALGO";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
-import { Form} from "react-bootstrap";
-//import { useState } from "react";
+import { Form } from "react-bootstrap";
+import { useState } from "react";
 
 
 
-const CreatorViews = ({appState, args, getParamsReady, getParams}) => {
-    
-            return(
-                getParamsReady
-                    ? <getParams functionToCall = {getParams}/>
-                    : <h1>Loading the GetParams page</h1>
-                );
+const CreatorViews = ({ appState, args, getParamsReady, getParams }) => {
+
+    return (
+        getParamsReady
+            ? <GetParams functionToCall={getParams} />
+            : <h1>Loading the GetParams page</h1>
+    );
 }
-const  getParams = ({ functionToCall }) => {
-        
+const GetParams = ({ functionToCall }) => {
 
-    const[lifetime, setlifetime] = this.setState(500);
-    const[price, setPrice] = this.setState(1);
-    const[id, setId]= this.setState();
+
+    const [lifetime, setlifetime] = useState(500);
+    const [price, setPrice] = useState(1);
+    const [id, setId] = useState(0);
+
     const handleClick = () => {
         functionToCall({
-            lifetime: lifetime, 
+            lifetime: lifetime,
             price: parseCurrency(price),
+            id: id
         });
     }
-            
-    return(
+
+    return (
         <Container>
             <Form.Group>
                 <p>Lifetime</p>
-                <Form.Control 
-                value = {lifetime}
-                onchange= {(e) => setlifetime(e.target.value)}
-                type = "Number"
-                placeholder = "Lifetime in blocks"/>
+                <Form.Control
+                    value={lifetime}
+                    onChange={(e) => setlifetime(e.target.value)}
+                    type="Number"
+                    placeholder="Lifetime in blocks" />
                 <p>Price</p>
-                <Form.Control 
-                value = {price}
-                onchange= {(e) => setPrice(e.target.value)}
-                type = "Number"
-                placeholder = "Price"/>
+                <Form.Control
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                    type="Number"
+                    placeholder="Price" />
                 <p>Id</p>
                 <Form.Control
-                value = {id}
-                onchange= {(e) => setId(e.target.value)}
-                type = "Number"
-                placeholder = "Id"/>
-            </Form.Group> 
-            <Button onClick = {handleClick}> Set Parameters</Button>
+                    value={id}
+                    onChange={(e) => setId(e.target.value)}
+                    type="Number"
+                    placeholder="Id" />
+            </Form.Group>
+            <Button onClick={handleClick}> Set Parameters</Button>
         </Container>
     );
 }
