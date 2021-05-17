59aa7baf3dc1a2ba31215275ca032ebf73171def
Caner Budak <62038536+CanerBudak@users.noreply.github.com>
2021-05-18 00:06:41 +0300

    Update OwnerViews.js

--- src/components/participants/OwnerViews.js
+++ src/components/participants/OwnerViews.js
@@ -5,29 +5,42 @@ import Spinner from "react-bootstrap/Spinner";
 import { useState } from "react";
 
 
-const OwnerViews = ({ appState, args, purchaseReadt, purchase}) => {
-    switch(appState) {
-        case "Purchase":
-            return(<Purchasing price={args[0]} functionToCall = {purchase}/>);
-        case "Id":
-            return(<revealId revealId={args[0]} functionToCall = {revealId}/>);
+const OwnerViews = ({ appState, args, purchaseReady, purchase }) => {
+    switch (appState) {
+        case "purchase":
+            return (purchaseReady ? <Purchasing price={args[0]} functionToCall={purchase} /> : <h2>Waiting</h2>);
+        case "seeOwner":
+            return (<SeeOwner owner={args[0]} />);
+        case "revealId":
+            return (<RevealId id={args[0]} />);
         default:
-            return(<h1>Waiting for contract</h1>);
+            return (<h1>Waiting for contract</h1>);
     }
 }
 
-const revealId = ({Id}) => {
+const SeeOwner = ({ owner }) => {
+    const address = owner.substring(0, 5) + "..." + owner.substring(owner.length - 4, owner.length - 1);
+
     return (
-        <Container>
-            <h2>`revealId: ${Id} `</h2>
+        <Container className="mt-4">
+            <h2>New Onwer: <strong>"{address}"</strong></h2>
 
         </Container>
     )
 }
-const Purchasing = ({ price, functionToCall}) => {
+
+const RevealId = ({ id }) => {
+    return (
+        <Container className="mt-4">
+            <h2>revealId: <strong>"{id}"</strong></h2>
+
+        </Container>
+    )
+}
+const Purchasing = ({ price, functionToCall }) => {
 
     const [clickedYes, setClickedYes] = useState(false);
-    const [clickedNo, setClickedNo] = useState(true);
+    const [clickedNo, setClickedNo] = useState(false);
 
     const handleYes = () => {
         functionToCall(true);
@@ -36,26 +49,28 @@ const Purchasing = ({ price, functionToCall}) => {
 
     const handleNo = () => {
         functionToCall(false);
-        setClickedNo(false);
+        setClickedNo(true);
     }
 
     return (
-        <Container>
+        <Container className="mt-4">
             <h2>Price is {price} ALGO. Do you accept it?</h2>
-            <Button onClick = {handleYes}>Yes</Button>
+            <Button onClick={handleYes}>
                 {
                     clickedYes
-                        ? <Spinner animation= "border" size= "sm" as="span" variant="Light"/>
+                        ? <Spinner animation="border" size="sm" as="span" variant="Light" />
                         : "Yes"
                 }
-            <Button onClick = {handleNo}>No</Button>
+            </Button>
+            <Button onClick={handleNo}>
                 {
                     clickedNo
-                        ? <Spinner animation= "border" size= "sm" as="span" variant="Light"/>
+                        ? <Spinner animation="border" size="sm" as="span" variant="Light" />
                         : "No"
                 }
+            </Button>
         </Container>
     )
 }
 
-export default OwnerViews; 
+export default OwnerViews;
